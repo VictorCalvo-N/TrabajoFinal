cf30d1570e1293c5836f8a3a76245db1b647db38
FabricioMessa <81938906+FabricioMessa@users.noreply.github.com>
2021-05-29 17:44:57 -0500

    Update Supermercado.cpp
    Nuevos cambios para presentar :D

--- Supermercado.cpp
+++ Supermercado.cpp
@@ -33,13 +33,12 @@ Cliente::Cliente(std::string _name, std::string _surname)
 
 void Cliente::comprarplus()
 {
-    std::cout << "Mi nombre es " << name << " " << surname << " y he venido a comprar varias cosas.\n";
+    std::cout << "Cliente: " << name << " " << surname << ". \n";
 }
 
-void Cliente::comprarless()
-{
-    std::cout << "Mi nombre es " << name << " " << surname << " y he venido a comprar pocas cosas.\n";
-}
+void productos(char);
+void opciones(int);
+void carrito(int, int);
 
 int main(){
 
@@ -53,10 +52,10 @@ int main(){
     std::string apellido;
     char respuesta0;
     char answer;
-    int pasillo;
-    int verduras;
-    char verdura[100];
-    char respuesta;
+    //int pasillo;
+    //int verduras;
+    //char verdura[100];
+    //char respuesta;
 
     std::cout << "Escriba su nombre: ";
     std::cin >> nombre;
@@ -65,147 +64,101 @@ int main(){
 
     Cliente c1 = Cliente(nombre, apellido);
 
-    do
-    {
-        std::cout << "Buenos dias, cuantas cosas va a comprar?(A.Varias / B.pocas): ";
-        std::cin >> respuesta0;
-    } while ((respuesta0 != 'A') && (respuesta0 != 'B'));
+    c1.comprarplus();
 
-    if (respuesta0 == 'A')
-    {
-        c1.comprarplus();
-    }
-    if (respuesta0 == 'B')
-    {
-        c1.comprarless();
-    }
-    
-    /*
-    do
-    {
-        std::cout << "Buenos dias, viene a comprar algo? (A. Buenos dias, si, vengo a comprar unas cosas para la semana./B. No, me equivoque de tienda, aios.): ";
-        std::cin >> answer;
+    std::cout << "\n";
 
-        if (answer == 'A')
-        {
-            std::cout << "Buenos dias, si, vengo a comprar unas cosas para la semana.\n"; 
-        }
-        if (answer == 'B')
-        {
-            std::cout << "No, me equivoque de tienda, aios.\n";
-        }
-    } while ((answer != 'A') && (answer != 'B'));
+    std::cout << "Buenos dias, viene a comprar algo? \n A. Buenos dias, si, vengo a comprar unas cosas para la semana.\n B. No, me equivoque de tienda, aios.: ";
+    std::cin >> answer;
 
     if (answer == 'A')
-    {   
-        std::cout << "==TENEMOS LOS SIGUIENTES PASILLOS DISPONIBLES==\n";
-        std::cout << "|||||||||||||\n";      
-        std::cout << "1. Verduras\n2. Carnes\n3. Frutas\n4. Pescado\n5. Dulces\n6. Juguetes";
-        std::cout << "\n|||||||||||||\n";
-
-    Return:
-
-        do
-        {
-            std::cout << "\nA que pasillo quisiera ir?: ";
-            std::cin >> pasillo;
-        } while ((pasillo != 1) && (pasillo != 2) && (pasillo != 3) && (pasillo != 4) && (pasillo != 5) && (pasillo != 6));
-
-        switch (pasillo)
-        {
-        case 1:
-            std::cout << "Bienvenido al pasillo de Verduras!\n";
-            std::cout << "==ESTAS SON LAS VERDURAS CON LAS QUE CONTAMOS EN ESTOS MOMENTOS==\n";
-            std::cout << "|||||||||||||\n";
-            std::cout << "1. Tomate\n2. Lechuga\n3. Col\n4. Papa\n5. Camote\n6. Yuca";
-            std::cout << "\n|||||||||||||\n";
-            
-            do
-            {
-                std::cout << "De estos productos, cuantos de aqui va a querer seleccionar?: ";
-                std::cin >> verduras;
-            } while ((verduras != 1) && (verduras != 2) && (verduras != 3) && (verduras != 4) && (verduras != 5) && (verduras != 6));
-
-            switch (verduras)
-            {
-            case 1:
-                std::cout << "Que verdura va a elegir?: ";
-                std::cin >> verdura;
-
-                do
-                {
-                    std::cout << "Entendido! Desea regresar a otro pasillo o solo comprar ese producto? (A.SI/ B.NO): ";
-                    std::cin >> respuesta;
-                } while ((respuesta != 'A') && (respuesta != 'B'));
-
-                if (respuesta == 'A')
-                {
-                    goto Return; 
-                }
-                if (respuesta == 'B')
-                {
-                    std::cout << "Vuelva pronto!!!";
-                }               
-                
-                break;
-            
-            case 2:
-                std::cout << "\nQue verduras va a elegir?: ";
-                break;
-
-            case 3:
-                std::cout << "\nQue verduras va a elegir?: ";
-                break;
-
-            case 4:
-                std::cout << "\nQue verduras va a elegir?: ";
-                break;
-
-            case 5:
-                std::cout << "\nQue verduras va a elegir?: ";
-                break;
-
-            case 6:
-                std::cout << "\nQue verduras va a elegir?: ";
-                break;
-
-            default:
-                break;
-            }
-
-            break;
-        
-        case 2:
-            std::cout << "Bienvenido al pasillo de Carnes!\n";
-            break;
-
-        case 3:
-            std::cout << "Bienvenido al pasillo de Frutas!\n";
-            break;
-
-        case 4:
-            std::cout << "Bienvenido al pasillo de Pescado!\n";
-            break;
-
-        case 5:
-            std::cout << "Bienvenido al pasillo de Dulces!\n";
-            break;
-
-        case 6:
-            std::cout << "Bienvenido al pasillo de Juguetes!\n";
-            break;    
-
-        default:
-            break;
-        }
+    {
+        productos(answer);
     }
     if (answer == 'B')
     {
-        std::cout << "Gracias, que tenga un buen dia.\n";
-    }
-    */   
+       system("pause");
+    }  
 
     system("pause");
     return 0;
 
+}
+
+void productos(char answer){
+    int opcion_productos;
+    if (answer == 'A')
+    {   
+        std::cout << "==TENEMOS LOS SIGUIENTES PRODUCTOS DISPONIBLES==\n";
+        std::cout << "|||||||||||||\n";      
+        std::cout << "1. Verduras\n2. Carnes\n3. Frutas\n4. Pescado\n5. Dulces\n6. Juguetes";
+        std::cout << "\n|||||||||||||\n";
+        std::cout << "Que producto desea?: ";
+        std::cin >> opcion_productos;
+        opciones(opcion_productos);
+    }
+    else if(answer == 'B')
+    {
+        std::cout << "Gracias, que tenga un buen dia.\n";
+        system("pause");
+    }
+}
+
+void opciones(int opcion_productos){
+    int verduras;
+    int cantidad_producto;
+
+    std::cout << "==ESTE ES NUESTRO STOCK==\n";
+
+    switch(opcion_productos){
+            case 1:
+            std::cout << "Bienvenido al pasillo de Verduras!\n";
+            std::cout << "|||||||||||||\n";
+            std::cout << "1. Tomate(Numero de Tomate)\n2. Lechuga(Numero de Lechuga)\n3. Col(Numero de Col)\n4. Papa(Numero de Papa)\n5. Camote(Numero de Camote)\n";
+            std::cout << "\n|||||||||||||\n";
+            std::cout << "Que verdura desea?: ";
+            std::cin >> verduras;
+            std::cout <<"Cuantas desea:? ";
+            std::cin >> cantidad_producto;
+            carrito(verduras,cantidad_producto);
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            default:
+                break;
+    }
+    std::cout << opcion_productos;
+}
+
+void carrito(int verduras, int cantidad_producto){
+    char decision;
+    char answer = 'A';
+    switch (verduras){
+        case 1:
+            std::cout << "Eligio la cantidad " << cantidad_producto << " de Tomates || Su costo final de esta operacion es: " << cantidad_producto * 0.5  << ".\n";
+            std::cout << "\nSe adjunta para la suma final";
+            do
+            {
+                std::cout << "\nDesea hacer otra operación?(S/N): ";
+                std::cin >> decision;
+                if (decision == 'S')
+                {
+                    productos(answer);
+                }
+                else if(decision == 'N')
+                {
+                    std::cout<<"Pasa a caja final";
+                    void caja_final();
+                }
+            } while ((decision != 'S') && (decision != 'N'));
+            
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        default:
+            break;
+    }
 }
